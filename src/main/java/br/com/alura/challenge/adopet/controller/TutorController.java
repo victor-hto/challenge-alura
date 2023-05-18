102303fa98ead678a0628c485a80d13cd8da7202
Victor Assemp <ti@assempbh.org.br>
2023-05-18 19:59:59 -0300

    Listagem de tutores

--- src/main/java/br/com/alura/challenge/adopet/controller/TutorController.java
+++ src/main/java/br/com/alura/challenge/adopet/controller/TutorController.java
@@ -3,12 +3,16 @@ package br.com.alura.challenge.adopet.controller;
 import br.com.alura.challenge.adopet.domain.Tutor;
 import br.com.alura.challenge.adopet.domain.dto.DadosAtualizarCadastroDto;
 import br.com.alura.challenge.adopet.domain.dto.DadosCadastroTutorDto;
+import br.com.alura.challenge.adopet.domain.dto.DadosListagemTutor;
 import br.com.alura.challenge.adopet.repository.TutorRepository;
 import br.com.alura.challenge.adopet.services.AtualizarTutorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tutores")
@@ -34,5 +38,15 @@ public class TutorController {
     public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizarCadastroDto dados) {
         var tutor = atualizarTutorService.atualizarTutor(dados);
         return ResponseEntity.ok(tutor);
+    }
+
+    @GetMapping
+    public ResponseEntity listar() {
+        var lista = tutorRepository.findAll();
+        if(lista.isEmpty()){
+            return ResponseEntity.ok("Nenhum item encontrado");
+        }
+
+        return ResponseEntity.ok(lista.stream().map(DadosListagemTutor::new).collect(Collectors.toList()));
     }
 }
